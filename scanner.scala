21da88161a8c2e98d3c34a4dcd4599ec5e6c29b5
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2023-05-11 14:43:17 +0200

    Remove redundant this references

--- scanner.scala
+++ scanner.scala
@@ -4,7 +4,7 @@ import scala.util.{Try, Success, Failure}
 
 class Scanner {
 
-    private[this] var line: Int = 1
+    private var line: Int = 1
 
     def scanTokens(source: String): Option[List[Token]]  = {
         if (source.size == 0) then None else
@@ -19,36 +19,36 @@ class Scanner {
         }
     }
 
-    private[this] def getToken(source: String): (Option[Token], String) = {
+    private def getToken(source: String): (Option[Token], String) = {
        source.toList match {
             case List() => (None, "")
             // Single character tokens.
-            case '('::rest => (Some(LeftParenToken(this.line)), rest.mkString)
-            case ')'::rest => (Some(RightParenToken(this.line)), rest.mkString)
-            case '{'::rest => (Some(LeftBraceToken(this.line)), rest.mkString)
-            case '}'::rest => (Some(RightBraceToken(this.line)), rest.mkString)
-            case ','::rest => (Some(CommaToken(this.line)), rest.mkString)
-            case '.'::rest => (Some(DotToken(this.line)), rest.mkString)
-            case '-'::rest => (Some(MinusToken(this.line)), rest.mkString)
-            case '+'::rest => (Some(PlusToken(this.line)), rest.mkString)
-            case ';'::rest => (Some(SemicolonToken(this.line)), rest.mkString)
-            case '*'::rest => (Some(StarToken(this.line)), rest.mkString)
+            case '('::rest => (Some(LeftParenToken(line)), rest.mkString)
+            case ')'::rest => (Some(RightParenToken(line)), rest.mkString)
+            case '{'::rest => (Some(LeftBraceToken(line)), rest.mkString)
+            case '}'::rest => (Some(RightBraceToken(line)), rest.mkString)
+            case ','::rest => (Some(CommaToken(line)), rest.mkString)
+            case '.'::rest => (Some(DotToken(line)), rest.mkString)
+            case '-'::rest => (Some(MinusToken(line)), rest.mkString)
+            case '+'::rest => (Some(PlusToken(line)), rest.mkString)
+            case ';'::rest => (Some(SemicolonToken(line)), rest.mkString)
+            case '*'::rest => (Some(StarToken(line)), rest.mkString)
  
             // Double character tokens.
-            case '!'::'='::rest => (Some(BangEqualToken(this.line)), rest.mkString)
-            case '!'::rest      => (Some(BangToken(this.line)), rest.mkString)
-            case '='::'='::rest => (Some(EqualEqualToken(this.line)), rest.mkString)
-            case '='::rest      => (Some(EqualToken(this.line)), rest.mkString)
-            case '<'::'='::rest => (Some(LessEqualToken(this.line)), rest.mkString)
-            case '<'::rest      => (Some(LessToken(this.line)), rest.mkString)
-            case '>'::'='::rest => (Some(GreaterEqualToken(this.line)), rest.mkString)
-            case '>'::rest      => (Some(GreaterToken(this.line)), rest.mkString)
+            case '!'::'='::rest => (Some(BangEqualToken(line)), rest.mkString)
+            case '!'::rest      => (Some(BangToken(line)), rest.mkString)
+            case '='::'='::rest => (Some(EqualEqualToken(line)), rest.mkString)
+            case '='::rest      => (Some(EqualToken(line)), rest.mkString)
+            case '<'::'='::rest => (Some(LessEqualToken(line)), rest.mkString)
+            case '<'::rest      => (Some(LessToken(line)), rest.mkString)
+            case '>'::'='::rest => (Some(GreaterEqualToken(line)), rest.mkString)
+            case '>'::rest      => (Some(GreaterToken(line)), rest.mkString)
             // Ignore a comment.
             case '/'::'/'::rest => (None, rest.dropWhile{ _ != '\n' }.mkString)
-            case '/'::rest      => (Some(SlashToken(this.line)), rest.mkString)
+            case '/'::rest      => (Some(SlashToken(line)), rest.mkString)
 
             // Ignored.
-            case '\n'::rest => this.line+=1; (None, rest.mkString)
+            case '\n'::rest => line+=1; (None, rest.mkString)
             case '\r'::rest => (None, rest.mkString)
             case '\t'::rest => (None, rest.mkString)
             case  ' '::rest => (None, rest.mkString)
@@ -60,23 +60,23 @@ class Scanner {
             // Identifiers and keywords.
             case s if s(0).isLetterOrDigit || s(0) == '_' => getIdentifierOrKeyword(s.mkString)
 
-            case unexpected::_ => Scalox.error(this.line, f"Illegal character: '${unexpected}'"); (None, "")
+            case unexpected::_ => Scalox.error(line, f"Illegal character: '${unexpected}'"); (None, "")
         }
     }
 
-    private[this] def getStringToken(rest: String): (Option[Token], String) = {
+    private def getStringToken(rest: String): (Option[Token], String) = {
         def consumeChars(acc: String, rest: String): (String, String) = {
             rest.toList match {
-                case List() | '\n'::_ => Scalox.error(this.line, f"Unterminated string"); ("", "")
+                case List() | '\n'::_ => Scalox.error(line, f"Unterminated string"); ("", "")
                 case '"'::rest        => (acc, rest.mkString)
                 case other::rest      => consumeChars(acc + other, rest.mkString)
             }
         }
         val (stringVal, left) = consumeChars("", rest)
-        (Some(StringToken(this.line, stringVal)), left)
+        (Some(StringToken(line, stringVal)), left)
     }
 
-    private[this] def getNumberToken(rest: String) = {
+    private def getNumberToken(rest: String) = {
         def consumeDigits(acc: String, rest: String): (String, String) = {
             rest.toList match {
                 case first::rest if first.isDigit || first == '.' => consumeDigits(acc + first, rest.mkString)
@@ -85,10 +85,10 @@ class Scanner {
         }
         
         val (digitVal, left) = consumeDigits("", rest)
-        (Some(NumberToken(this.line, digitVal.toDouble)), left)
+        (Some(NumberToken(line, digitVal.toDouble)), left)
     }
 
-    private[this] def getIdentifierOrKeyword(source: String): (Option[Token], String)  = {
+    private def getIdentifierOrKeyword(source: String): (Option[Token], String)  = {
         def takeAlphanum(acc: String, rest: String): (String, String) = {
             rest.toList match {
                 case List()                                            => (acc, "")
@@ -100,29 +100,29 @@ class Scanner {
         val (unidentified, rest) = takeAlphanum("", source)
         unidentified match {
             // Keywords.
-            case "class" => (Some(ClassToken(this.line)), rest)
-            case "else"  => (Some(ElseToken(this.line)), rest)
-            case "false" => (Some(FalseToken(this.line)), rest)
-            case "fun"   => (Some(FunToken(this.line)), rest)
-            case "for"   => (Some(ForToken(this.line)), rest)
-            case "if"    => (Some(IfToken(this.line)), rest)
-            case "nil"   => (Some(NilToken(this.line)), rest)
-            case "or"    => (Some(OrToken(this.line)), rest)
-            case "print" => (Some(PrintToken(this.line)), rest)
-            case "return"=> (Some(ReturnToken(this.line)), rest)
-            case "super" => (Some(SuperToken(this.line)), rest)
-            case "this"  => (Some(ThisToken(this.line)), rest)
-            case "true"  => (Some(TrueToken(this.line)), rest)
-            case "var"   => (Some(VarToken(this.line)), rest)
-            case "while" => (Some(WhileToken(this.line)), rest)
+            case "class" => (Some(ClassToken(line)), rest)
+            case "else"  => (Some(ElseToken(line)), rest)
+            case "false" => (Some(FalseToken(line)), rest)
+            case "fun"   => (Some(FunToken(line)), rest)
+            case "for"   => (Some(ForToken(line)), rest)
+            case "if"    => (Some(IfToken(line)), rest)
+            case "nil"   => (Some(NilToken(line)), rest)
+            case "or"    => (Some(OrToken(line)), rest)
+            case "print" => (Some(PrintToken(line)), rest)
+            case "return"=> (Some(ReturnToken(line)), rest)
+            case "super" => (Some(SuperToken(line)), rest)
+            case "this"  => (Some(ThisToken(line)), rest)
+            case "true"  => (Some(TrueToken(line)), rest)
+            case "var"   => (Some(VarToken(line)), rest)
+            case "while" => (Some(WhileToken(line)), rest)
             // Identifier.
-            case _       => (Some(IdentifierToken(this.line, unidentified)), rest)
+            case _       => (Some(IdentifierToken(line, unidentified)), rest)
         }
     }
 }
 
 abstract class Token {
-    override def toString = this.getClass.getSimpleName
+    override def toString = getClass.getSimpleName
 }
 case class LeftParenToken(line: Int)  extends Token
 case class RightParenToken(line: Int) extends Token
@@ -135,6 +135,7 @@ case class PlusToken(line: Int)       extends Token
 case class SemicolonToken(line: Int)  extends Token
 case class SlashToken(line: Int)      extends Token
 case class StarToken(line: Int)       extends Token
+
 // One or two character tokens.
 case class BangToken(line: Int)         extends Token
 case class BangEqualToken(line: Int)    extends Token
@@ -144,16 +145,18 @@ case class GreaterToken(line: Int)      extends Token
 case class GreaterEqualToken(line: Int) extends Token
 case class LessToken(line: Int)         extends Token
 case class LessEqualToken(line: Int)    extends Token
+
 // Literals.
 case class IdentifierToken(line: Int, value: String) extends Token {
-    override def toString = f"${this.getClass.getSimpleName} ${value}"
+    override def toString = f"${getClass.getSimpleName} ${value}"
 }
 case class StringToken(line: Int, value: String)     extends Token {
-    override def toString = f"${this.getClass.getSimpleName} '${value}'"
+    override def toString = f"${getClass.getSimpleName} '${value}'"
 }
 case class NumberToken(line: Int, value: Double)     extends Token {
-    override def toString = f"${this.getClass.getSimpleName} ${value}"
+    override def toString = f"${getClass.getSimpleName} ${value}"
 }
+
 // Keywords.
 case class AndToken(line: Int)    extends Token
 case class ClassToken(line: Int)  extends Token
