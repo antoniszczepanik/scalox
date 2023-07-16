b39e3b12e7cf2621fcbb4a453836897c6d4a4e57
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2023-07-16 14:32:25 +0200

    Interpret all basic operators for Doubles

--- interpreter.scala
+++ interpreter.scala
@@ -2,12 +2,62 @@ package scalox
 
 type Value = String | Boolean | Double | Null
 
+def getType(value: Value): String = value match {
+  case _: String => "String"
+  case _: Boolean => "Boolean"
+  case _: Double => "Double"
+  case null => "Null"
+}
+
 class LiteralVal(val value: Value) {
-  def -(other: LiteralVal): LiteralVal = {
-    (value, other.value) match {
-      case (a: Double, b: Double) => LiteralVal(a - b)
-      case (a, b) => throw PanicException(-1, s"substraction not defined for $a and $b")
-    }
+  def +(other: LiteralVal): LiteralVal = (value, other.value) match {
+    case (a: Double, b: Double) => LiteralVal(a + b)
+    case (a, b) => throw PanicException(-1, s"addition not defined for ${getType(a)} and ${getType(b)}")
+  }
+
+  def -(other: LiteralVal): LiteralVal = (value, other.value) match {
+    case (a: Double, b: Double) => LiteralVal(a - b)
+    case (a, b) => throw PanicException(-1, s"substraction not defined for ${getType(a)} and ${getType(b)}")
+  }
+
+  def *(other: LiteralVal): LiteralVal = (value, other.value) match {
+    case (a: Double, b: Double) => LiteralVal(a * b)
+    case (a, b) => throw PanicException(-1, s"multiplication not defined for ${getType(a)} and ${getType(b)}")
+  }
+
+  def /(other: LiteralVal): LiteralVal = (value, other.value) match {
+    case (a: Double, b: Double) => LiteralVal(a / b)
+    case (a, b) => throw PanicException(-1, s"division not defined for ${getType(a)} and ${getType(b)}")
+  }
+
+  def ==(other: LiteralVal): LiteralVal = (value, other.value) match {
+    case (a: Double, b: Double) => LiteralVal(a == b)
+    case (a, b) => throw PanicException(-1, s"cannot check equality for ${getType(a)} and ${getType(b)}")
+  }
+
+  def !=(other: LiteralVal): LiteralVal = (value, other.value) match {
+    case (a: Double, b: Double) => LiteralVal(a != b)
+    case (a, b) => throw PanicException(-1, s"cannot check equality for ${getType(a)} and ${getType(b)}")
+  }
+
+  def <(other: LiteralVal): LiteralVal = (value, other.value) match {
+    case (a: Double, b: Double) => LiteralVal(a < b)
+    case (a, b) => throw PanicException(-1, s"cannot check equality for ${getType(a)} and ${getType(b)}")
+  }
+
+  def <=(other: LiteralVal): LiteralVal = (value, other.value) match {
+    case (a: Double, b: Double) => LiteralVal(a <= b)
+    case (a, b) => throw PanicException(-1, s"cannot check equality for ${getType(a)} and ${getType(b)}")
+  }
+
+  def >(other: LiteralVal): LiteralVal = (value, other.value) match {
+    case (a: Double, b: Double) => LiteralVal(a > b)
+    case (a, b) => throw PanicException(-1, s"cannot check equality for ${getType(a)} and ${getType(b)}")
+  }
+
+  def >=(other: LiteralVal): LiteralVal = (value, other.value) match {
+    case (a: Double, b: Double) => LiteralVal(a >= b)
+    case (a, b) => throw PanicException(-1, s"cannot check equality for ${getType(a)} and ${getType(b)}")
   }
 }
 
@@ -23,18 +73,18 @@ object Interpreter {
       }
 
       case Binary(expr1, op, expr2) => op match {
-        case MinusToken(_) => interpret(expr1) - interpret(expr2)
-        case binary => throw PanicException(-1, s"unimplemented: interpret $binary")
-        // case PlusToken(_)         => interpret(expr) + interpret(expr)
-        // case SlashToken(_)        => interpret(expr) / interpret(expr) 
-        // case StarToken(_)         => interpret(expr) * interpret(expr)
-        // case EqualEqualToken(_)   => interpret(expr) == interpret(expr)
-        // case BangEqualToken(_)    => interpret(expr) != interpret(expr)
-        // case LessToken(_)         => interpret(expr) < interpret(expr)
-        // case LessEqualToken(_)    => interpret(expr) <= interpret(expr)
-        // case GreaterToken(_)      => interpret(expr) > interpret(expr)
-        // case GreaterEqualToken(_) => interpret(expr) >= interpret(expr)
+        case MinusToken(_)        => interpret(expr1) - interpret(expr2)
+        case PlusToken(_)         => interpret(expr1) + interpret(expr2)
+        case SlashToken(_)        => interpret(expr1) / interpret(expr2) 
+        case StarToken(_)         => interpret(expr1) * interpret(expr2)
+        case EqualEqualToken(_)   => interpret(expr1) == interpret(expr2)
+        case BangEqualToken(_)    => interpret(expr1) != interpret(expr2)
+        case LessToken(_)         => interpret(expr1) < interpret(expr2)
+        case LessEqualToken(_)    => interpret(expr1) <= interpret(expr2)
+        case GreaterToken(_)      => interpret(expr1) > interpret(expr2)
+        case GreaterEqualToken(_) => interpret(expr1) >= interpret(expr2)
       }
+
       case Grouping(expr) => interpret(expr)
     }
   }
